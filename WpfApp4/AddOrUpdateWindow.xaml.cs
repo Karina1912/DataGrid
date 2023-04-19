3d6c2b68345ece936838aa433b082bb1cc59a880
Karina1912 <karinashcherbakova96@gmail.com>
2023-04-19 21:02:43 +0700

    first commit

---
+++ WpfApp4/AddOrUpdateWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp4
+{
+    /// <summary>
+    /// Логика взаимодействия для AddOrUpdateWindow.xaml
+    /// </summary>
+    public partial class AddOrUpdateWindow : Window
+    {
+        public AddOrUpdateWindow()
+        {
+            InitializeComponent();
+            Load();
+            DataContext = new Users();
+        }
+        public AddOrUpdateWindow(Users user)
+        {
+            InitializeComponent();
+            Load();
+            DataContext = user;
+        }
+        private void Load()
+        {
+            CmbRoles.ItemsSource = Helper.context.Roles.ToList();
+        }
+        private void BtnSubmit_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (DataContext is Users user && user.Id == 0)
+            {
+                Helper.context.Users.Add(user);
+            }
+            Helper.context.SaveChanges();
+            Close();
+        }
+    }
+}
---
+++ WpfApp4/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp4
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Load();
+        }
+        private void Load()
+        {
+            UsersDataGrid.ItemsSource = Helper.context.Users.ToList();
+        }
+
+        private void BtnRemove_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (UsersDataGrid.SelectedItem is Users user)
+            {
+                Helper.context.Users.Remove(user);
+                Helper.context.SaveChanges();
+                Load();
+            }
+        }
+
+        private void BtnAdd_OnClick(object sender, RoutedEventArgs e)
+        {
+            new AddOrUpdateWindow().ShowDialog();
+            Load();
+        }
+
+        private void BtnUpdate_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (UsersDataGrid.SelectedItem is Users user)
+            {
+                new AddOrUpdateWindow(user).ShowDialog();
+                Load();
+            }
+        }
+    }
+}
